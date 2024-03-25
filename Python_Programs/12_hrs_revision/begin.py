213ea175402c1156731f9b8c1f357764cdd3eb53
Aarushi <aarushiajay4@gmail.com>
2024-03-25 13:16:38 +0530

    Revision

---
+++ Python_Programs/12_hrs_revision/begin.py
@@ -0,0 +1,55 @@
+rows = int(input("How many rows"))
+coloumns = int(input("How many coloumns"))
+
+symbol= input("Which symbol")
+
+for i in range(rows):
+    for i in range(coloumns):
+        print(symbol,end="")
+    print()
+
+
+# import math 
+# import time
+
+# for i in range(10,0,-1):
+#     print(i)
+#     time.sleep(1)
+
+# print("Happy New Year")
+# pi = 3.14
+
+# print(round(pi))
+
+# print(math.atan(pi))
+
+# print(math.ceil(pi))
+# name= "Aarushi"
+
+# print("Hello",name)
+
+# print(type(name))
+
+# name = "bro Code"
+
+
+# print(len(name))
+# print(name.find("o"))
+# print(name.capitalize())
+# print(name.upper())
+# print(name.lower())
+# print(name.isdigit())
+# print(name*3)
+
+
+# gender = "girl"
+
+# age = 3
+
+# if gender == "girl" and age == 3:
+#     print("you passed")
+# else:
+#     print("you failed")
+
+# for i in range(10):
+#     print(i)
