eaaa8419a011878e697e64e60273c5baf4e94714
Aarushi <aarushiajay4@gmail.com>
2024-05-01 23:56:26 +0530

    empty

--- Practice-Models/100-days-python/Day16.py
+++ Practice-Models/100-days-python/Day16.py
@@ -1,20 +1,20 @@
-class Person:
+# class Person:
     # name = "Harry"
     # occupation = "Engineer"
     # networth = 10
-    def __init__(self,n,o):
-        print("Hi am contructor")
-        self.name = n
-        self.occupation = o
+#     def __init__(self,n,o):
+#         print("Hi am contructor")
+#         self.name = n
+#         self.occupation = o
 
-    def info(self):
-        print(f"{self.name} is a {self.occupation}")
+#     def info(self):
+#         print(f"{self.name} is a {self.occupation}")
 
-a = Person("Harry","Develper")
-b = Person("Hari","HR")
+# a = Person("Harry","Develper")
+# b = Person("Hari","HR")
 
-a.info()
-b.info()
+# a.info()
+# b.info()
 
 # # a.name = "Shubham"
 # # a.networth = 20
@@ -22,4 +22,28 @@ b.info()
 # # print(a.name,a.networth)
 # a.info()
 
+
+# def greet(fx):
+#     def mfx():
+#         print("Good Morning")
+#         fx()
+#         print("Thanks for using it")
+#     return mfx
+
+# @greet
+# def hello():
+#     print("Hello World")
+
+# hello()
+
+class MyClass:
+    def __init__(self):
+        self.value = Value
+    
+    def show(self):
+        print(f"Value is {self._value}")
+    
+    @property
+    def value(self):
+        return self._value
 
