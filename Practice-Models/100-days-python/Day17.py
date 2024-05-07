81fc45bd28dac7e1000ff70a81712b2df1149e36
Aarushi <aarushiajay4@gmail.com>
2024-05-07 07:28:01 +0530

    Another one

--- Practice-Models/100-days-python/Day17.py
+++ Practice-Models/100-days-python/Day17.py
@@ -1,1 +1,19 @@
 #only for streak
+class Employee:
+    def __init__(self,name,id):
+        self.name = name
+        self.id =id
+
+    def showDetails(self):
+        print(f"The name is {self.name} & id is {self.id}")
+
+class Pogrammer(Employee):
+    def showLanguage(self):
+        print("The default language is python")
+
+
+e1 = Employee("Aarushi", 30)
+e1.showDetails()
+e2 = Pogrammer("Vivek",92)
+e2.showDetails()
+e2.showLanguage()
